9b2f4deb2c2a7eb3e1174a690c757f9df8e23260
Mehdi Rezaie <rezaie@aerimus.phy.ohiou.edu>
2019-02-21 15:45:45 -0500

    do regression on DR7

---
+++ src/mult_fit.py
@@ -0,0 +1,190 @@
+'''
+    This code 
+    1. takes an observation data
+    2. fits the ngal/nbar vs. a set of systematics linear or quadratic
+    3. contaminates a set of mocks (plot an example of the contamination model)
+    4. look at the ngal histrogram from the mocks and compare it with the observation    
+'''
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+plt.rc('font', family='serif')
+import fitsio as ft
+import numpy as np
+from scipy.optimize import curve_fit
+
+def quad(x, *theta):
+    return theta[0] + np.matmul(x, np.array(theta[1:]))
+def lin(x, *theta):
+    return theta[0] + np.matmul(x, np.array(theta[1:]))
+
+class DATA(object):
+    def __init__(self, INPUT, ax=list(range(18)), OUTPUT='./dr5linquadfit', split=False):
+        self.ax = ax
+        if split:
+            datai = np.load(INPUT).item()
+            data  = np.concatenate([datai['test']['fold'+str(i)] for i in range(5)])
+        else:
+            data = ft.read(INPUT)
+        self.hpix = data['hpind']
+        self.XSTATS = (np.mean(data['features'], axis=0), np.std(data['features'], axis=0))
+        self.f = (data['features']-self.XSTATS[0])/self.XSTATS[1]
+        if len(ax)==1:
+            self.X = self.f[:,self.ax]#.squeeze()
+            self.XX = np.concatenate([self.X, self.X*self.X], axis=1)
+        else:
+            self.X = self.f[:,self.ax]
+            self.XX = np.concatenate([self.X, self.X*self.X], axis=1)
+        #self.Y  = data['label']/np.average(data['label'], weights=data['fracgood'])
+        self.Y  = data['label']
+        self.Ye = (1./data['fracgood'])**0.5        
+        log  = '! ---- Fit a linear/quadratic multivariate to data ------\n'
+        log += 'reading {} \n'.format(INPUT)
+        log += 'number of data : {} Number of systematic maps : {}\n'.format(*data['features'].shape)
+        log += 'feature indices to be used : {}\n'.format(ax)
+        popt, pcov = curve_fit(quad, self.XX, self.Y, p0=[0 for i in range(2*len(ax)+1)],
+                                 sigma=self.Ye, method='lm', absolute_sigma=True)
+        self.quad  = (popt, pcov)
+        popt, pcov = curve_fit(lin, self.X, self.Y, p0=[0 for i in range(len(ax)+1)],
+                                 sigma=self.Ye, method='lm', absolute_sigma=True)
+        self.lin   = (popt, pcov)
+        rmse = lambda y1, y2, noise: np.sqrt(np.mean(((y1-y2)/noise)**2))
+        #
+        #
+        baseline_model = np.mean(self.Y)
+        rmse_baseline  = rmse(baseline_model, self.Y, self.Ye)
+        rmse_lin       = rmse(lin(self.X, *self.lin[0]), self.Y, self.Ye)
+        rmse_quad      = rmse(quad(self.XX, *self.quad[0]), self.Y, self.Ye)
+        log += 'baseline model has RMSE of {}\n'.format(rmse_baseline)
+        log += 'linear fit has RMSE of {}\n'.format(rmse_lin)
+        log += 'quadratic fit has RMSE of {}\n'.format(rmse_quad)
+        log += 'write results on {}'.format(OUTPUT)
+        oudata = {'rmses':[rmse_baseline, rmse_lin, rmse_quad], 'log':log,
+                  'params':{'lin':self.lin, 'quad':self.quad}, 'baseline':baseline_model,
+                  'xstats':self.XSTATS, 'ax':self.ax}
+        np.save(OUTPUT, oudata)
+        print(log) # show results
+        
+        
+        
+    def plotbfitparam(self, path4png='bfitdr5.pdf'):
+        print('plotting bfit plot under {}'.format(path4png))
+        c = ['cornflowerblue','crimson']
+        self.LABELS = ['EBV', 'logHI', 'NStar']+[l+s for l in ['depth','seeing','skymag','exptime', 'mjd'] for s in 'rgz']
+        labels = [self.LABELS[i] for i in self.ax]
+        f = plt.figure(figsize=(10,5))
+        plt.subplots_adjust(hspace=0.1)
+        gs  = gridspec.GridSpec(2, 1, height_ratios=[1, 4])
+        ax1 = plt.subplot(gs[0])
+        ax2 = plt.subplot(gs[1])
+        ax1.set_ylim(0.81, 1.1)
+        ax1.errorbar(np.arange(len(self.lin[0])), self.lin[0], np.diag(self.lin[1])**0.5,
+                     marker='.', color=c[1], ls='-')
+        ax1.errorbar(np.arange(len(self.quad[0])), self.quad[0], np.diag(self.quad[1])**0.5,
+                     marker='+', color=c[0], ls=':')
+        ax1.axhline(0, ls='--', color='k',alpha=0.5)
+        ax1.xaxis.tick_top()
+        ax1.spines['bottom'].set_visible(False)
+        ax1.spines['top'].set_visible(False)
+        ax1.spines['right'].set_visible(False)
+        ax1.set_xticks([])
+        ax1.text(0.8, 0.5, 'quadratic', color=c[0], transform=ax1.transAxes, size=20)
+        ax1.text(0.8, 0.0, 'linear', color=c[1], transform=ax1.transAxes, size=20)
+        #ax1.grid()
+        ax2.spines['top'].set_visible(False)
+        ax2.spines['right'].set_visible(False)
+        plt.xticks(np.arange(2*len(self.ax)+1), ['b']+[l+s for s in ['','$^{2}$'] for l in labels], rotation=90)
+        ax2.errorbar(np.arange(len(self.lin[0])), self.lin[0], np.diag(self.lin[1])**0.5,
+                     marker='.', color=c[1], ls='-')
+        ax2.errorbar(np.arange(len(self.quad[0])), self.quad[0], np.diag(self.quad[1])**0.5,
+                     marker='+', color=c[0], ls=':')
+        ax2.set_ylim(-0.06, 0.07)
+        ax2.axhline(0, color='k', ls='--', alpha=0.5)
+        ax2.set_ylabel(r'$\Theta_{i}$', size=18)
+        #ax2.grid()
+        d = 0.01
+        kwargs = dict(transform=ax1.transAxes, color='k', clip_on=False)
+        ax1.plot((-d, +d), (-d/2, +d/2), **kwargs)        # top-left diagonal
+        kwargs.update(transform=ax2.transAxes)            # switch to the bottom axes
+        ax2.plot((-d, +d), (1 - d/2, 1 + d/2), **kwargs)  # bottom-left diagonal
+        plt.savefig(path4png, bbox_inches='tight', dpi=300)
+        
+    def plotnnbardata(self, path4nnbar='nnbardr5fit.pdf'):
+        print('plotting nnbar plot under {}'.format(path4nnbar))        
+        from scipy.stats import binned_statistic
+        def binit(x, values=None):
+            y1, x1, _ = binned_statistic(x, values=values)
+            n1, _, _ = binned_statistic(x, values=values, statistic='count')
+            s1, _, _ = binned_statistic(x, values=values, statistic=np.std)
+            x  = x1[:-1] 
+            ye = s1/np.sqrt(n1)
+            mask = (y1 != 0.0 ) & ~np.isnan(y1) & (ye !=0.0) & (~np.isnan(ye))
+            return y1[mask], x[mask], ye[mask]
+        # 
+        f,a = plt.subplots(ncols=3, nrows=6,
+                          figsize=(15, 30), sharey=True)
+        plt.subplots_adjust(wspace=0.02)
+        a = a.flatten()
+        f.delaxes(a[-1])
+        c = ['grey','crimson', 'cornflowerblue']
+        x = self.f*self.XSTATS[1] + self.XSTATS[0]
+        inds = np.random.choice(np.arange(x.shape[0]), size=1000, replace=False)
+        for i in range(x.shape[1]):
+            a[i].set_xlabel(self.LABELS[i])
+            a[i].set_ylim(0,2)
+            # scatters
+            a[i].scatter(x[inds,i], self.Y[inds], color=c[0], alpha=0.02, marker='.') # data
+            a[i].scatter(x[inds,i], lin(self.X[inds], *self.lin[0]), 
+                         color=c[1], alpha=0.02, marker='+')
+            a[i].scatter(x[inds,i],quad(self.XX[inds], *self.quad[0]),
+                         color=c[2], alpha=0.02, marker='*')
+            # binned
+            y1, x1, s1 = binit(x[:,i], values=lin(self.X, *self.lin[0]))
+            a[i].errorbar(x1, y1, yerr=s1, color=c[1], marker='+', ls=':')
+            y1, x1, s1 = binit(x[:,i], values=quad(self.XX, *self.quad[0]))
+            a[i].errorbar(x1, y1, yerr=s1, color=c[2], marker='*', ls='--')
+            y1, x1, s1 = binit(x[:,i], values=self.Y)
+            a[i].errorbar(x1, y1, yerr=s1, color=c[0], marker='.', ls='-')
+            if i%3==0:a[i].set_ylabel(r'Ngal/$\overline{Ngal}$')
+        # add legends        
+        for j,(c,l) in enumerate(zip(c, ['DR5 data', 'linear-fit', 'quadratic-fit'])):
+            a[0].text(0.5, 0.94-j*0.06, l, color=c, transform=a[0].transAxes)
+        plt.savefig(path4nnbar, bbox_inches='tight', dpi=300)
+    def savemodels(self, path4models):
+        print('saving  '+path4models+'lin-weights.hp256.fits')
+        print('saving  '+path4models+'quad-weights.hp256.fits')
+        import healpy as hp
+        lin_model  = lin(self.X, *self.lin[0])
+        quad_model = quad(self.XX, *self.quad[0])
+        mapi = np.zeros(12*256**2)
+        mapi[self.hpix] = lin_model
+        hp.write_map(path4models+'lin-weights.hp256.fits', mapi, fits_IDL=False, dtype=np.float64, overwrite=True)
+        mapj = np.zeros(12*256**2)
+        mapj[self.hpix] = quad_model
+        hp.write_map(path4models+'quad-weights.hp256.fits', mapj, fits_IDL=False, dtype=np.float64, overwrite=True)
+#
+from argparse import ArgumentParser
+ap = ArgumentParser(description='Multivariate linear/quadratic regression')
+ap.add_argument('--input',  default='/Volumes/TimeMachine/data/DR7/eBOSS.ELG.NGC.DR7.table.5.r.npy')
+ap.add_argument('--ax',     nargs='*', type=int, default=[i for i in range(18)])
+ap.add_argument('--output', default='/Volumes/TimeMachine/data/DR7/results/regression/')
+ap.add_argument('--split',  action='store_true')
+ap.add_argument('--plots',  action='store_true')
+ns = ap.parse_args()
+
+print('INPUTS :')
+dics = ns.__dict__
+for keyi in dics.keys():
+    print('%20s : %s'%(keyi, dics[keyi]))
+
+import os 
+if not os.path.exists(ns.output):
+    os.makedirs(ns.output)
+    
+fitdata = DATA(ns.input, ns.ax, ns.output+'regression_log', split=ns.split)
+if ns.plots:
+   fitdata.plotbfitparam(ns.output+'bfit_params')
+   fitdata.plotnnbardata(ns.output+'nnbar.pdf')
+
+fitdata.savemodels(ns.output) # only address
---
+++ src/nn_fit.py
@@ -0,0 +1,468 @@
+'''
+    Regression with a Feed Forward Neural Network
+    (c) mehdi rezaie
+    Feb 21, 2019 
+'''
+import os
+import numpy as np
+import sys
+import NN
+
+from time import time
+
+
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+
+
+def getcf(d):
+    from scipy.stats import pearsonr
+    # lbl = ['ebv', 'nstar'] + [''.join((s, b)) for s in ['depth', 'seeing', 'airmass', 'skymag', 'exptime'] for b in 'rgz']
+    cflist = []
+    indices = []
+    for i in range(d['train']['fold0']['features'].shape[1]):
+        for j in range(5):
+            fold = ''.join(['fold', str(j)])
+            cf = pearsonr(d['train'][fold]['label'], d['train'][fold]['features'][:,i])[0]
+            if np.abs(cf) >= 0.02:
+                #print('{:s} : sys_i: {} : cf : {:.4f}'.format(fold, lbl[i], cf))
+                indices.append(i)
+                cflist.append(cf)
+    if len(indices) > 0:
+        indices = np.unique(np.array(indices))
+        return indices
+    else:
+        print('no significant features')
+        return None
+#     cf = []
+#     indices = []
+#     for i in range(features.shape[1]):
+#         cf.append(pearsonr(label, features[:,i]))
+#         if np.abs(cf) > 0.0
+   
+def get_all(ablationlog):
+    d = np.load(ablationlog).item()
+    indices = None
+    for il, l in enumerate(d['validmin']):
+       m = (np.array(l) - d['RMSEall']) > 0.0
+       #print(np.any(m), np.all(m))
+       if np.all(m):
+         #print(il, d['indices'][il])
+         #print(il, [lbs[m] for m in d['indices'][il]])
+         #break
+         indices = d['indices'][il]
+         break
+       if (il == len(d['validmin'])-1) & (np.any(m)):
+          indices = [d['indices'][il][-1]]       
+    # return either None or indices
+    return indices
+ 
+   
+    
+if rank == 0:
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+    plt.rc('font', family='serif')
+    plt.rc('text', usetex=True)
+    #
+    #
+    import os 
+    import fitsio as ft
+    import healpy as hp
+    from argparse import ArgumentParser
+    ap = ArgumentParser(description='Neural Net regression')
+    ap.add_argument('--input',  default='test_train_eboss_dr5-masked.npy')
+    ap.add_argument('--output', default='test_train_eboss_dr5-maskednn.npy')
+    ap.add_argument('--nside',  default=256, type=int)
+    ap.add_argument('--axfit',  nargs='*',   type=int,\
+                                default=[i for i in range(18)])
+    ap.add_argument('--ablog',  default='None')
+    ns = ap.parse_args()
+    NSIDE = ns.nside
+    config = dict(nchain=1, batchsize=1024, nepoch=100, Units=[0],
+              tol=0.0, scale=0.0, learning_rate=0.001)
+    # scales : 1000, 100, 50, 20, 10, 1, 5
+    #
+    log  = '! ===== Regression with Neural Net ======\n'
+    log += 'reading input : {} with nside : {} \n'.format(ns.input, NSIDE)
+    log += 'the fiducial config is {}\n'.format(config)
+    data   = np.load(ns.input).item()
+    if ns.ablog == 'cf':
+       axfit  = getcf(data)
+    elif os.path.isfile(ns.ablog):
+       axfit  = get_all(ns.ablog)
+    elif ns.ablog == 'None':
+       axfit  = ns.axfit
+    else:
+       RaiseError("axis is not set correctly!")
+    oupath = ns.output
+    if not os.path.exists(ns.output):
+       os.makedirs(ns.output)
+else:
+    oupath = None
+    data   = None
+    config = None
+    axfit  = None
+
+data   = comm.bcast(data,   root=0)
+oupath = comm.bcast(oupath, root=0)
+config = comm.bcast(config, root=0)
+axfit  = comm.bcast(axfit,  root=0)
+
+if rank == 0:
+    if axfit is None:
+        print('Rank %d : no correction is required!!! '%rank)
+        print('Rank %d : will make a constant map ... '%rank)
+        oumap  = np.ones(12*NSIDE*NSIDE)
+        hp.write_map(oupath+'nn-weights.hp'+str(NSIDE)+'.fits', oumap, dtype=np.float64, fits_IDL=False, overwrite=True)
+        sys.exit()
+    else:
+        print('Rank {} : will carry on with {} features '.format(rank, axfit))
+else:
+    if axfit is None:
+        sys.exit()
+    else:
+        print('Rank %d : will carry on ...'%rank)
+
+
+assert size == 5, 'required number of mpi tasks should be equal to number of folds'
+train_i = data['train']['fold'+str(rank)]
+test_i  = data['test']['fold'+str(rank)]
+valid_i = data['validation']['fold'+str(rank)]
+
+
+
+
+if rank==0:log+='Rank %d : train %d validation %d and test %d \n'%(rank, train_i.size, test_i.size, valid_i.size)
+comm.Barrier()
+
+
+#
+# train Num of Layers
+#
+valid_min  = []
+nlayers = [[0], [40], [20,20], [20, 10, 10], [10, 10, 10, 10]] #  
+if rank==0:
+     log+='start the hyper-parameter training with the initial config {}\n'.format(config)
+     log+='training num of layers {} \n'.format(nlayers)
+for nl_i in nlayers:
+     t1  = time()
+     config.update(Units=nl_i)
+     net = NN.Netregression(train_i, valid_i, test_i, axfit)
+     net.train_evaluate(**config)
+     rmse = []
+     for a in net.epoch_MSEs:
+         rmse.append(np.sqrt(a[2][:,2]))
+     RMSE  = np.column_stack(rmse)
+     RMSEm = np.mean(RMSE, axis=1)
+     #RMSEe = np.std(RMSE, axis=1)/np.sqrt(RMSE.shape[1])
+     baseline = np.sqrt(net.optionsdic['baselineMSE'][1])
+     #valid_rmse[str(nl_i)] = [net.epoch_MSEs[0][2][:,0], RMSEm/baseline, RMSEe/baseline]
+     valid_min.append(np.min(RMSEm)/baseline)
+     if rank==0:log+='rank{} finished {} in {} s\n'.format(rank, nl_i, time()-t1)
+     #plt.plot(np.arange(RMSEm.size), RMSEm/baseline, label='{}'.format(nl_i))
+comm.Barrier()
+# #if rank ==0:
+# #  plt.legend()
+# #  plt.ylim(0.95, 1.05)
+# #  plt.show() 
+# #sys.exit()
+# # valid_rmse = comm.gather(valid_rmse, root=0)
+valid_min  = comm.gather(valid_min, root=0)
+
+if rank == 0:
+     VMIN = np.array(valid_min)
+     Mean = VMIN.mean(axis=0)
+     argbest = np.argmin(Mean)
+     x = np.arange(VMIN.shape[1])
+     plt.figure(figsize=(4,3))
+     for i in range(VMIN.shape[0]):
+         plt.plot(x, VMIN[i,:], color='grey', ls=':', alpha=0.5)
+     plt.plot(x, Mean, color='red', ls='-', label='Average across the folds')
+     plt.axvline(x[argbest], ls='--', color='k', label='Capacity : CV estimate')
+     plt.xticks(x, [str(l) for l in nlayers], rotation=45)
+     plt.ylabel(r'$RMSE_{NN}/RMSE_{baseline}$')
+     plt.xlabel('Capacity')
+     plt.legend()
+     nlbest = nlayers[argbest]
+     config.update(Units=nlbest)
+     log += 'best nlayers is :: {}\n'.format(nlbest)
+     log += 'the updated config is {}\n'.format(config)
+     plt.savefig(oupath+'nlayers_validation.pdf', bbox_inches='tight')
+else:
+     config = None    
+config = comm.bcast(config, root=0)
+if rank == 0:print(config)
+    
+
+#
+# train learning rate scale
+#
+valid_min  = []
+lrates = np.array([0.0001, 0.001, 0.01])
+if rank==0:log+='training learning rate {} \n'.format(lrates)
+for lrate in lrates:
+    t1 = time()
+    config.update(learning_rate=lrate)
+    net = NN.Netregression(train_i, valid_i, test_i, axfit)
+    net.train_evaluate(**config)
+    rmse = []
+    for a in net.epoch_MSEs:
+        rmse.append(np.sqrt(a[2][:,2]))
+    RMSE  = np.column_stack(rmse)
+    RMSEm = np.mean(RMSE, axis=1)
+    #RMSEe = np.std(RMSE, axis=1)/np.sqrt(RMSE.shape[1])
+    baseline = np.sqrt(net.optionsdic['baselineMSE'][1])
+    valid_min.append(np.min(RMSEm)/baseline)
+    if rank==0:log+='rank{} finished {} in {} s\n'.format(rank, lrate, time()-t1)
+comm.Barrier()
+valid_min  = comm.gather(valid_min, root=0)
+if rank == 0:
+    VMIN = np.array(valid_min)
+    Mean = VMIN.mean(axis=0)
+    argbest = np.argmin(Mean)
+    plt.figure(figsize=(4,3))
+    for i in range(VMIN.shape[0]):
+        plt.plot(-np.log10(lrates), VMIN[i,:], color='grey', ls=':', alpha=0.5)
+    #
+    plt.plot(-np.log10(lrates), Mean, color='red', ls='-', label='Average across the folds')
+    plt.axvline(-np.log10(lrates[argbest]), ls='--', color='k', label=r'Learning Rate : CV estimate')
+    plt.ylabel(r'$RMSE_{NN}/RMSE_{baseline}$')
+    plt.xlabel(r'$-\log(Learning Rate)$')
+    plt.legend()    
+    lrbest = lrates[argbest]
+    config.update(learning_rate=lrbest)
+    log += 'best learning rate is :: {}\n'.format(lrbest)
+    log += 'the updated config is {}\n'.format(config)
+    plt.savefig(oupath+'learningrate_validation.pdf', bbox_inches='tight')
+else:
+    config = None    
+config = comm.bcast(config, root=0)    
+if rank == 0:print(config)
+#
+# train batchsize
+#
+valid_min  = []
+bsizes = np.array([512, 1024, 2048, 4096])
+if rank==0:log+='training batchsize {} \n'.format(bsizes)
+for bsize in bsizes:
+     t1 = time()
+     config.update(batchsize=bsize)
+     net = NN.Netregression(train_i, valid_i, test_i, axfit)
+     net.train_evaluate(**config)
+     rmse = []
+     for a in net.epoch_MSEs:
+         rmse.append(np.sqrt(a[2][:,2]))
+     RMSE  = np.column_stack(rmse)
+     RMSEm = np.mean(RMSE, axis=1)
+     #RMSEe = np.std(RMSE, axis=1)/np.sqrt(RMSE.shape[1])
+     baseline = np.sqrt(net.optionsdic['baselineMSE'][1])
+     valid_min.append(np.min(RMSEm)/baseline)
+     if rank==0:log+='rank{} finished {} in {} s\n'.format(rank, bsize, time()-t1)
+comm.Barrier()
+valid_min  = comm.gather(valid_min, root=0)
+if rank == 0:
+     VMIN = np.array(valid_min)
+     Mean = VMIN.mean(axis=0)
+     argbest = np.argmin(Mean)
+     plt.figure(figsize=(4,3))
+     for i in range(VMIN.shape[0]):
+         plt.plot(np.log10(bsizes), VMIN[i,:], color='grey', ls=':', alpha=0.5)
+
+     plt.plot(np.log10(bsizes), Mean, color='red',   ls='-', label='Average across the folds')
+     plt.axvline(np.log10(bsizes[argbest]), ls='--', color='k', label=r'Batch Size : CV estimate')
+     plt.ylabel(r'$RMSE_{NN}/RMSE_{baseline}$')
+     plt.xlabel(r'$\log(Batch Size)$')
+     plt.legend()    
+     bsbest = bsizes[argbest]
+     config.update(batchsize=bsbest)
+     log +='best Batchsize is :: {}\n'.format(bsbest)
+     log += 'the updated config is {}\n'.format(config)
+     plt.savefig(oupath+'batchsize_validation.pdf', bbox_inches='tight')
+else:
+     config = None     
+config = comm.bcast(config, root=0)        
+if rank == 0:print(config)    
+
+#
+# train regularization scale
+#
+valid_min  = []
+scales = np.exp([-50.0, -40.0, -30.0, -25.0, -20.0, -15.0, -10.0, -5.0, 0.0])
+if rank==0:log+='training reg. scale {} \n'.format(scales)
+for scale_i in scales:
+     t1 = time()
+     config.update(scale=scale_i)
+     net = NN.Netregression(train_i, valid_i, test_i, axfit)
+     net.train_evaluate(**config)
+     rmse = []
+     for a in net.epoch_MSEs:
+         rmse.append(np.sqrt(a[2][:,2]))
+     RMSE  = np.column_stack(rmse)
+     RMSEm = np.mean(RMSE, axis=1)
+     #RMSEe = np.std(RMSE, axis=1)/np.sqrt(RMSE.shape[1])
+     baseline = np.sqrt(net.optionsdic['baselineMSE'][1])
+     valid_min.append(np.min(RMSEm)/baseline)
+     if rank==0: log+='rank{} finished {} in {} s\n'.format(rank, scale_i, time()-t1)
+
+comm.Barrier()
+valid_min  = comm.gather(valid_min, root=0)
+if rank == 0:
+     VMIN = np.array(valid_min)
+     Mean = VMIN.mean(axis=0)
+     argbest = np.argmin(Mean)
+     plt.figure(figsize=(4,3))
+     for i in range(VMIN.shape[0]):
+         plt.plot(-np.log(scales), VMIN[i,:], color='grey', ls=':', alpha=0.5)
+     #
+     plt.plot(-np.log(scales), Mean, color='red', ls='-', label='Average across the folds')
+     plt.axvline(-np.log(scales[argbest]), ls='--', color='k', label=r'$\lambda$ : CV estimate')
+     plt.ylabel(r'$RMSE_{NN}/RMSE_{baseline}$')
+     plt.xlabel(r'$-\ln(\lambda)$')
+     plt.legend()
+     sclbest = scales[argbest]
+     config.update(scale=sclbest)
+     log +='best scale is :: {}\n'.format(sclbest)
+     log += 'the updated config is {}\n'.format(config)
+     plt.savefig(oupath + 'scales_validation.pdf', bbox_inches='tight')
+else:
+     config = None
+config = comm.bcast(config, root=0)
+
+if rank == 0:print(config)
+#    
+# train number of epochs
+#
+valid_min  = []
+nepoch_max = 400
+nepochs = np.arange(nepoch_max + 1)
+config.update(nepoch=nepoch_max)
+if rank==0:log+='training num of epochs up to {} \n'.format(nepoch_max)
+t1 = time()
+net = NN.Netregression(train_i, valid_i, test_i, axfit)
+net.train_evaluate(**config)
+rmse = []
+for a in net.epoch_MSEs:
+    rmse.append(np.sqrt(a[2][:,2]))
+RMSE  = np.column_stack(rmse)
+RMSEm = np.mean(RMSE, axis=1)
+#RMSEe = np.std(RMSE, axis=1)/np.sqrt(RMSE.shape[1])
+baseline = np.sqrt(net.optionsdic['baselineMSE'][1])
+valid_min = RMSEm/baseline
+if rank==0: log+='rank{} finished {} in {} s\n'.format(rank, nepochs[-1], time()-t1)
+
+comm.Barrier()
+valid_min  = comm.gather(valid_min, root=0)
+if rank == 0:
+     VMIN = np.array(valid_min)
+     Mean = VMIN.mean(axis=0)
+     argbest = np.argmin(Mean)
+     plt.figure(figsize=(4,3))
+     ymin = 1.e6
+     for i in range(VMIN.shape[0]):
+         plt.plot(nepochs, VMIN[i,:], color='grey', ls=':', alpha=0.5)
+         ymin = np.minimum(np.min(VMIN[i, :]), ymin)
+     #
+     plt.plot(nepochs, Mean, color='red', ls='-', label='Average across the folds')
+     plt.axvline(nepochs[argbest], ls='--', color='k', label=r'Number of Epochs : CV estimate')
+     plt.ylabel(r'$RMSE_{NN}/RMSE_{baseline}$')
+     plt.xlabel(r'Number of Epochs')
+     plt.ylim(0.8*ymin, 1.2*ymin)
+     plt.legend()    
+     nebest = nepochs[argbest]
+     config.update(nepoch=nebest)
+     config.update(nchain=10)
+     log += 'best nepoch is :: {}\n'.format(nebest)
+     log += 'the updated config is {}\n'.format(config)
+     plt.savefig(oupath+'nepochs_validation.pdf', bbox_inches='tight')
+else:
+     config = None    
+config = comm.bcast(config, root=0)
+if rank == 0:print(config)
+
+if rank==0:
+   log+='final run for the best hyper-parameters\n'
+   log+='BHPS: {}\n'.format(config)
+
+t1    = time()
+net   = NN.Netregression(train_i, valid_i, test_i, axfit)
+net.train_evaluate(**config)
+
+rmse  = []
+for a in net.epoch_MSEs:
+    rmse.append(np.sqrt(a[2][:,2]))
+
+RMSE        = np.column_stack(rmse)
+RMSEm       = np.mean(RMSE, axis=1)
+RMSEe       = np.std(RMSE, axis=1)/np.sqrt(RMSE.shape[1])
+baseline    = np.sqrt(net.optionsdic['baselineMSE'][1])
+rmselist    = [net.epoch_MSEs[0][2][:,0], RMSEm/baseline, RMSEe/baseline]
+meanY, stdY = net.optionsdic['stats']['ystat']
+predP       = net.test.P
+#predY = stdY*net.test.Y.squeeze()+meanY # true input label
+y_avg = []
+for yi in net.chain_y:
+    y_avg.append(yi[1].squeeze().tolist())    
+predY = stdY*np.mean(np.array(y_avg), axis=0) + meanY
+ 
+
+
+if rank==0: log+='finished final run for rank {} in {} s \n'.format(rank, time()-t1)
+
+comm.Barrier()
+rmselist = comm.gather(rmselist, root=0)
+predY    = comm.gather(predY, root=0)
+predP    = comm.gather(predP, root=0)
+if rank ==0:
+    hpix   = np.concatenate(predP)
+    ngal   = np.concatenate(predY)
+    oudata = np.zeros(ngal.size, dtype=[('hpind', 'i8'), ('weight','f8')])
+    oudata['hpind']   = hpix
+    oudata['weight'] = ngal
+    oumap  = np.zeros(12*NSIDE*NSIDE)
+    oumap[hpix] = ngal
+    np.save(oupath+'nn-rmse', rmselist)
+    ft.write(oupath+'nn-weights'+str(NSIDE)+'.fits', oudata)
+    hp.write_map(oupath+'nn-weights.hp'+str(NSIDE)+'.fits', oumap, fits_IDL=False, overwrite=True, dtype=np.float64)
+    log += 'write rmses in {}\n'.format(oupath+'nn-rmse')
+    log += 'write hpix.weight in {}\n'.format(oupath+'nn-weights'+str(NSIDE)+'.fits')
+    log += 'write weight map in {}\n'.format(oupath+'nn-weights.hp'+str(NSIDE)+'.fits')
+    #
+    #
+    ymin = 1.e6
+    plt.figure(figsize=(4,3))
+    for l in range(len(rmselist)):
+        x  = rmselist[l][0]
+        y  = rmselist[l][1]
+        ye = rmselist[l][2]
+        plt.fill_between(x, y-ye, y+ye, color='grey', alpha=0.1)
+        plt.plot(x, y, color='grey')
+        ymin = np.minimum(np.min(y), ymin)
+
+    plt.xlabel('Number of Epochs')
+    plt.ylabel(r'$RMSE_{NN}/RMSE_{baseline}$')
+    plt.ylim(0.8*ymin, 1.2*ymin)
+    plt.savefig(oupath+'nn-rmse.pdf', bbox_inches='tight')
+    log += 'write the rmse plot in {}'.format(oupath+'nn-rmse.pdf')
+    logfile = open(oupath+'nn-log.txt', 'w')
+    logfile.write(log)
+    #
+    #
+'''
+t1 = time()
+net = NN.Netregression(data['train']['fold0'], data['validation']['fold0'], data['test']['fold0'])
+net.train_evaluate(nchain=1, batchsize=1000, nepoch=300, Units=[4, 4], learning_rate=1.e-3)
+print('done in {} s'.format(time()-t1))
+
+plt.plot(net.epoch_MSEs[0][2][:,0], net.epoch_MSEs[0][2][:,1], 'k-')
+plt.plot(net.epoch_MSEs[0][2][:,0], net.epoch_MSEs[0][2][:,2], 'k--')
+plt.ylabel('MSE')
+plt.xlabel('Training epoch')
+plt.ylim(1.0,1.5)
+
+
+print('RMSE for baseline : %.3f NN : %.3f'%(np.sqrt(net.optionsdic['baselineMSE'][2]), np.sqrt(net.epoch_MSEs[0][1])))
+'''
