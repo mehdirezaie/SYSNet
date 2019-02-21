84114948a7c94d6d3e20cc0000127e275dc0d60d
Mehdi Rezaie <rezaie@aerimus.phy.ohiou.edu>
2019-02-21 15:48:07 -0500

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # SYSNet
-Neural Network modeling of imaging systematics in Galaxy Surveys
+## Neural Network modeling of imaging systematics in Galaxy Surveys
+
+In this project we develop a Feed Forward Neural Network to model the relationship between the imaging maps eg. Stellar density and the observed galaxy density field, in order to mitigate for the systematic effects and to make a robust galaxy clustering measurements.
+
+The hyper parameters of the neural network is trained with 5-fold Validation technique. The cost function is Mean Squared Error and a L2 regularization term, and the optimization algorithm is Adaptive Moment (ADAM).
+
+The strengths and weaknesses of the method is investigated using three versions of simulated datasets:
+
+Mocks without any contamination: to assess how much bias and variance is introduced in absence of any systematic effects
+Mocks with small contamination : a quadratic function of a single map
+Mocks with large contamination : TBD
+We use the Ablation technique to reduce the dimensionality of the problem, and minimize the likelihood of regressing out the cosmological clustering signal.
--- bash/analyze_dr7.sh
+++ bash/analyze_dr7.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 source activate py3p6
 
+# codes
 ablation=/Users/rezaie/github/SYSNet/src/ablation.py
+multfit=/Users/rezaie/github/SYSNet/src/mult_fit.py
+nnfit=/Users/rezaie/github/SYSNet/src/nn_fit.py
+
+# output dirs & labels
 glmp5=/Volumes/TimeMachine/data/DR7/eBOSS.ELG.NGC.DR7.table.5.r.npy
 oudr_ab=/Volumes/TimeMachine/data/DR7/results/ablation/
+oudr_r=/Volumes/TimeMachine/data/DR7/results/regression/
+mult1=mult_all
+mult2=mult_depz
 log_ab=dr7.log
+nn1=nn_ab
 
-
+# ================ RUNS ====================
+# REGRESSION
+#
 # Feb 20: Ablation on DR7
-#mpirun --oversubscribe -np 5 python $ablation --data $glmp5 --output $oudr_ab --log $log_ab
+# mpirun --oversubscribe -np 5 python $ablation --data $glmp5 --output $oudr_ab --log $log_ab
 # took 54 min
 
- 
+# Feb 21: Linear/quadratic multivariate fit on DR7 
+#         Linear/quadratic depth-z fit on DR7
+#         NN fit on DR7 with ablation
+#
+# python $multfit --input $glmp5 --output ${oudr_r}${mult1}/ --split
+# python $multfit --input $glmp5 --output ${oudr_r}${mult2}/ --split --ax 5
+# took around 10 secs
+#
+mpirun --oversubscribe -np 5 python $nnfit --input $glmp5 --output ${oudr_r}${nn1}/ --ablog ${oudr_ab}${log_ab}.npy
+
+
+# CLUSTERING
+#
+
