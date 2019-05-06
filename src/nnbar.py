fix the normalization in cell estimator, run nn without ablation,

src/nnbar.py
@@ -25,7 +25,7 @@ class NNBAR(object):
         # digitize
         if binning == 'simple':
             bins = np.linspace(self.sysmap.min(), self.sysmap.max(), nbins+1) 
-            self.sysl   = [0 for k in range(2*bins.size)]
+            self.sysl   = [0 for k in range(2*nbins)]
             inds = np.digitize(self.sysmap, bins)
             for i in range(1,bins.size): # what if there is nothing on the last bin? FIXME
                 self.sysl[2*i-2] = self.galmap[np.where(inds == i)].tolist()
@@ -33,10 +33,17 @@ class NNBAR(object):
         elif binning == 'equi-area':
             npts  = self.ranmap.size
             swtt  = self.ranmap.sum()/nbins  # num of randoms in each bin
-            ss, gs, ws = zip(*sorted(zip(self.sysmap, self.galmap, self.ranmap)))
+            datat = np.zeros(self.sysmap.size, dtype=np.dtype([('ss', 'f8'), ('gs', 'f8'), ('ws', 'f8'), ('rid', 'i8')]))
+            datat['ss'] = self.sysmap
+            datat['gs'] = self.galmap
+            datat['ws'] = self.ranmap
+            datat['rid'] = np.random.choice(np.arange(self.sysmap.size), size=self.sysmap.size, replace=False)
+            datas = np.sort(datat, order=['ss', 'rid'])
+            ss, gs, ws = datas['ss'], datas['gs'], datas['ws']
+            #ss, gs, ws = zip(*sorted(zip(self.sysmap, self.galmap, self.ranmap)))
             swti = 0.0
             i   = 0
-            self.sysl = [0 for k in range(2*nbins+2)] 
+            self.sysl = [0 for k in range(2*nbins)] 
             listg = []
             listr = []
             bins  = [ss[0]] # first edge is the lowest systematic
@@ -57,8 +64,8 @@ class NNBAR(object):
             bins = np.array(bins)
             print('min sys : %.2f  max sys : %.2f'%(ss[0], ss[npts-1]))
             print('num of pts : %d, num of bins : %d'%(i, j))
-        self.avnden = np.sum([np.sum(self.sysl[i]) for i in np.arange(0,2*bins.size, 2)])\
-                      /np.sum([np.sum(self.sysl[i]) for i in np.arange(1,2*bins.size, 2)])
+        self.avnden = np.sum([np.sum(self.sysl[i]) for i in np.arange(0,2*nbins, 2)])\
+                      /np.sum([np.sum(self.sysl[i]) for i in np.arange(1,2*nbins, 2)])
         self.bins = bins
         
     def run(self, njack=20):

src/run_pipeline.py
@@ -328,9 +328,14 @@ if ns.clfile != 'none':
         delta_ngal = makedelta(galmap, ranmap, mask, select_fun=wmap)
         #
         # compute C_l
+        map_i  = hp.ma(mask.astype('f8')) 
+        map_i.mask = np.logical_not(mask) 
+        clmask = hp.anafast(map_i.filled(), lmax=ns.lmax)
+        sf = ((2*np.arange(clmask.size)+1)*clmask).sum()/(4.*np.pi)
+  
         map_ngal       = hp.ma(delta_ngal * ranmap)
         map_ngal.mask  = np.logical_not(mask)
-        cl_auto        = hp.anafast(map_ngal.filled(), lmax=ns.lmax)
+        cl_auto        = hp.anafast(map_ngal.filled(), lmax=ns.lmax) / sf
         #
         if ns.njack == 0:
             cl_err = 0.0
@@ -358,9 +363,11 @@ if ns.clfile != 'none':
         map_ngal  = None
         hpix      = None
         makedelta = None
+        sf        = None
        
     #
     # bcast
+    sf         = comm.bcast(sf, root=0)
     x          = comm.bcast(x, root=0)
     map_ngal   = comm.bcast(map_ngal, root=0)
     mask       = comm.bcast(mask, root=0)
@@ -388,7 +395,7 @@ if ns.clfile != 'none':
         map_sys      = ma(delta_sys * ranmap)
         map_sys.mask = np.logical_not(mask)
         my_cl_i      = anafast(map_ngal.filled(), map2=map_sys.filled(),\
-                               lmax=ns.lmax)
+                               lmax=ns.lmax)/sf
         my_cl.append(my_cl_i)
     #
     #
@@ -407,7 +414,7 @@ if ns.clfile != 'none':
         plt.xscale('log');plt.legend(ncol=2, bbox_to_anchor=(1.01,1.01))
         plt.ylabel(r'C$_{l}$');plt.xlabel('l')
         plt.savefig(ns.oudir + ns.clfile + '.png', bbox_inches='tight', dpi=300)
-        All_cl = dict(cross=all_cl, auto=cl_auto, clerr=cl_err, clabels=[labels[n] for n in ns.axfit])
+        All_cl = dict(cross=all_cl, auto=cl_auto, clerr=cl_err, clabels=[labels[n] for n in ns.axfit], sf=sf)
         np.save(ns.oudir  + ns.clfile, All_cl)
         log   += '{:35s} : {}\n'.format('Outpus saved under', ns.oudir)
         fo.write(log)
@@ -429,6 +436,11 @@ if ns.clsys != 'none':
         mask2     = np.zeros_like(mask1).astype('bool')
         mask2[feat['hpind']] = True
         mask      = mask1 & mask2         # overlap 
+        # get the mask Cl
+        map_i  = hp.ma(mask.astype('f8')) 
+        map_i.mask = np.logical_not(mask) 
+        clmask = hp.anafast(map_i.filled(), lmax=ns.lmax)
+        sf = ((2*np.arange(clmask.size)+1)*clmask).sum()/(4.*np.pi)
         
         ranmap    = hp.read_map(ns.ranmap, verbose=False)
         #
@@ -447,6 +459,7 @@ if ns.clsys != 'none':
         log += '{:35s} : {}\n'.format('Total number of overlap mask', mask.sum())      
         log += '{:35s} : {}\n'.format('Compute the auto power spectra for systematics', [labels[n] for n in ns.axfit])
     else:
+        sf        = None
         x         = None
         ranmap    = None
         mask      = None
@@ -455,6 +468,7 @@ if ns.clsys != 'none':
        
     #
     # bcast
+    sf         = comm.bcast(sf, root=0)
     x          = comm.bcast(x, root=0)
     mask       = comm.bcast(mask, root=0)
     ranmap     = comm.bcast(ranmap, root=0)
@@ -480,7 +494,7 @@ if ns.clsys != 'none':
         delta_sys    = makedelta(sys_i, ranmap, mask, select_fun=None, is_sys=True)
         map_sys      = ma(delta_sys * ranmap)
         map_sys.mask = np.logical_not(mask)
-        my_cl_i      = anafast(map_sys.filled(), lmax=ns.lmax)
+        my_cl_i      = anafast(map_sys.filled(), lmax=ns.lmax) / sf
         my_cl.append(my_cl_i)
     #
     #
@@ -499,7 +513,7 @@ if ns.clsys != 'none':
         plt.xscale('log');plt.legend(ncol=2, bbox_to_anchor=(1.01,1.01))
         plt.ylabel(r'C$_{l}$');plt.xlabel('l')
         plt.savefig(ns.oudir + ns.clsys + '.png', bbox_inches='tight', dpi=300)
-        All_cl = dict(cross=all_cl, auto=None, clabels=[labels[n] for n in ns.axfit])
+        All_cl = dict(cross=all_cl, auto=None, clabels=[labels[n] for n in ns.axfit], sf=sf)
         np.save(ns.oudir  + ns.clsys, All_cl)
         log   += '{:35s} : {}\n'.format('Outpus saved under', ns.oudir)
         fo.write(log)
@@ -587,12 +601,12 @@ if ns.corfile != 'none':
     #
     my_i  = rank*my_size
     my_f  = np.minimum(x.shape[1], (rank+1)*my_size)
-    my_x  = x[:, my_i:my_f]
+    print('rank %d : start %d  end %d'%(rank, my_i, my_f))
     my_xi = []
     for i in range(my_i, my_f):
         sys_i        = np.zeros_like(ranmap)
         sys_i[hpix]  = x[:, i]
-        my_xi_i      = run_XI(None, galmap, ranmap, wmap, mask, sysm=sys_i, njack=ns.njack, Return=True)
+        my_xi_i      = run_XI(None, galmap, ranmap, wmap, mask, sysm=sys_i, njack=0, Return=True) # no jackknife for cross
         my_xi.append(my_xi_i)
     #
     #

src/utils.py
@@ -49,12 +49,20 @@ def histedges_equalN(x, nbin=10, kind='size', weight=None):
                      np.arange(npt),
                      np.sort(x))
     elif kind == 'area':
+        sys.exit('FIX this routine for a repetitave x')
         npt1  = len(x)-1
         sumw = np.sum(weight) / nbin
         i    = 0
         wst  = 0.0
         xp   = [x.min()]  # lowest bin is the minimum
-        xs, ws =  zip(*sorted(zip(x, weight)))
+        #
+        #
+        datat        = np.zeros(x.size, dtype=np.dtype([('x', 'f8'), ('w', 'f8'), ('rid', 'i8')]))
+        datat['x']   = x
+        datat['w']   = weight
+        datat['rid'] = np.random.choice(np.arange(x.size), size=x.size, replace=False)
+        datas  = np.sort(datat, order=['x', 'rid'])
+        xs, ws = datas['x'], datas['w'] #zip(*sorted(zip(x, weight)))
         for wsi in ws:
             wst += wsi
             i   += 1
@@ -115,7 +123,22 @@ def clerr_jack(delta, mask, weight, njack=20, lmax=512):
     dummy = np.ones(mask.sum())
     hpixl, wl, deltal,_ = split_jackknife(hpix, weight[mask], 
                                           delta[mask], dummy, njack=njack)
+    print('# of jackknifes %d, input : %d'%(len(hpixl), njack))
     cljks = {}
+    # get the cl of the jackknife mask
+    wlt = wl.copy()
+    hpixt   = hpixl.copy()
+    wlt.pop(0)
+    hpixt.pop(0)
+    wlc = np.concatenate(wlt)
+    hpixc  = np.concatenate(hpixt)
+    maski  = np.zeros(npix, '?')
+    maski[hpixc] = True 
+    map_i  = hp.ma(maski.astype('f8'))
+    map_i.mask = np.logical_not(maski)
+    clmaskj = hp.anafast(map_i.filled(), lmax=lmax)
+    sfj = ((2*np.arange(clmaskj.size)+1)*clmaskj).sum()/(4.*np.pi) 
+
     for i in range(njack):
         hpixt   = hpixl.copy()
         wlt     = wl.copy()
@@ -139,7 +162,7 @@ def clerr_jack(delta, mask, weight, njack=20, lmax=512):
         #
         map_i       = hp.ma(deltai * wlci)
         map_i.mask  = np.logical_not(maski)
-        cljks[i]    = hp.anafast(map_i.filled(), lmax=lmax)
+        cljks[i]    = hp.anafast(map_i.filled(), lmax=lmax)/sfj
     #
     hpixt   = hpixl.copy()
     wlt     = wl.copy()
@@ -157,15 +180,20 @@ def clerr_jack(delta, mask, weight, njack=20, lmax=512):
     deltai[hpixc]  = deltac
     wlci[hpixc]    = wlc
     #
+    map_i      = hp.ma(maski.astype('f8'))
+    map_i.mask = np.logical_not(maski)
+    clmask = hp.anafast(map_i.filled(), lmax=lmax)
+    sf = ((2*np.arange(clmask.size)+1)*clmask).sum()/(4.*np.pi) 
+
     map_i       = hp.ma(deltai * wlci)
     map_i.mask  = np.logical_not(maski)
-    cljks[-1]    = hp.anafast(map_i.filled(), lmax=lmax)   # entire footprint
+    cljks[-1]   = hp.anafast(map_i.filled(), lmax=lmax)/sf   # entire footprint
     #
     clvar = np.zeros(len(cljks[-1]))
     for i in range(njack):
         clvar += (cljks[-1] - cljks[i])*(cljks[-1] - cljks[i])
     clvar *= (njack-1)/njack
-    return dict(clerr=np.sqrt(clvar), cljks=cljks)
+    return dict(clerr=np.sqrt(clvar), cljks=cljks, clmaskj=clmaskj, clmask=clmask, sf=sf, sfj=sfj)
 
 
 
@@ -211,7 +239,7 @@ def split_jackknife(hpix, weight, label, features, njack=20):
             w_l     = []
             hpix_l = []
             label_l = []
-        elif i == hpix.size-1:
+        elif (i == hpix.size-1) and (frac > 0.9*f):
             hpix_L.append(hpix_l)
             frac_L.append(frac)
             label_L.append(label_l)

src/xi.py
@@ -53,7 +53,7 @@ def split_jackknife(theta, phi, weight, delta, sysm, njack=20):
             theta_l = []
             phi_l   = []
             delta_l = []
-        elif i == theta.size-1:
+        elif (i == theta.size-1) and (frac > 0.9*f):
             theta_L.append(theta_l)
             phi_L.append(phi_l)
             frac_L.append(frac)
@@ -87,6 +87,7 @@ class XI_JACK(object):
         w = ranmap[mask]
         theta, phi  = hp.pix2ang(self.nside, np.argwhere(mask).flatten())
         thetal, phil, wl, deltal,_ = split_jackknife(theta, phi, w, delta[mask], delta[mask], njack=njack)
+        print('input njack %d, output njack %d'%(njack, len(wl)))
         self.theta  = thetal
         self.phi    = phil
         self.weight = wl